600e6e1d19aebb6555f60dcb6d51ede548b9093f
ATXLtheAxolotl <49346095+ATXLtheAxolotl@users.noreply.github.com>
2021-08-12 14:05:14 -0400

    Updated Main.cs

--- Main.cs
+++ Main.cs
@@ -1,4 +1,4 @@
-﻿using MelonLoader;
+using MelonLoader;
 using UnityEngine;
 using TMPro;
 
@@ -10,7 +10,7 @@ namespace FPSmod
         public const string Description = "Mod for counting how many frames you have."; // Description for the Mod.  (Set as null if none)
         public const string Author = "ATXLtheAxolotl"; // Author of the Mod.  (MUST BE SET)
         public const string Company = null; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "0.0.1"; // Version of the Mod.  (MUST BE SET)
+        public const string Version = "0.0.2"; // Version of the Mod.  (MUST BE SET)
         public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
     }
 
@@ -24,15 +24,22 @@ namespace FPSmod
         int interval = 1;
         float nextTime = 0;
         public int fpsDisplay;
+        public bool startDisplaying;
+
+        public override void OnApplicationStart() 
+        {
+            startDisplaying = false;
+        }
 
         public override void OnSceneWasLoaded(int buildindex, string sceneName) // Runs when a Scene has Loaded and is passed the Scene's Build Index and Name.
         {
+            startDisplaying = true;
             menuBoard = GameObject.Find("MenuBoard");
             userID = GameObject.Find("MenuBoard/UserID");
             background = GameObject.Find("MenuBoard/Background");
             clonedID = GameObject.Instantiate(userID);
             clonedID.transform.SetParent(menuBoard.transform, false);
-            clonedID.transform.position = new Vector3(-0.08f, 92.26f, 3.0491f);
+            clonedID.transform.position = new Vector3(-0.08f, 92.66f, 3.0491f);
             clonedID.transform.localScale = new Vector3(2f, 2f, 2f);
             clonedID.transform.SetAsFirstSibling();
             background.transform.SetAsFirstSibling();
@@ -40,13 +47,12 @@ namespace FPSmod
 
         public override void OnUpdate()
         {
-            if (Time.time >= nextTime)
-            {
-
-                GetFPS();
-
-                nextTime += interval;
-
+            if(startDisplaying == true) {
+                if (Time.time >= nextTime)
+                {
+                    GetFPS();
+                    nextTime += interval;
+                }
             }
         }
         public void GetFPS()
